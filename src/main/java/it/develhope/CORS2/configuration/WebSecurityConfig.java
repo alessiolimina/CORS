a61b21a9a19f3ae1479bad78cf18c0e208273557
Alessio Limina <alessio.limina@gmail.com>
2023-05-31 16:02:23 +0200

    Exercise not working

---
+++ src/main/java/it/develhope/CORS2/configuration/WebSecurityConfig.java
@@ -0,0 +1,29 @@
+package it.develhope.CORS2.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebMvc
+public class WebSecurityConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors().and().authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated())
+                .httpBasic(withDefaults());
+
+        return http.build();
+    }
+}
